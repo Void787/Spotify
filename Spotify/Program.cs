758f10f3505653d1e3e91d764aa3a0b5c5b301f3
Moenir <1186234@student.roc-nijmegen.nl>
2023-06-19 14:53:31 +0200

    album pause

--- Spotify/Program.cs
+++ Spotify/Program.cs
@@ -8,10 +8,12 @@
             Console.WriteLine("Hello, World!");
  
 
-                Artiest artiest = new Artiest( null,null, "balls");
+                Artiest artiest = new Artiest( null,null, "the man");
+                Artiest artiest2 = new Artiest(null, null, "the women");
                 Client client = new Client( null, null, null,null);
                 Song song = new Song ("the titel", 9.23 ,"a gernre", artiest);
-                client.Pause(song);
+                Album album = new Album("titel", 9.28, null, null);
+                album.Pause(song);
 
                 Console.ReadLine(); 
             }
